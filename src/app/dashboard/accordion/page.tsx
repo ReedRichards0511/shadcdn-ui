5c71417dae7bb7c71d5b0e379e4302eab8f739d9
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2024-07-02 16:00:27 -0500

    add acordion component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,8 +1,41 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accesible",
+    answer: "Yes. It is adheres to the WAI_ARIA desgin pattern."
+  },
+  {
+    id: "item-2",
+    question: "How do I get started",
+    answer: "You can start by reading the documentation."
+  },
+  {
+    id: "item-3",
+    question: "Can I use it on multiple projects?",
+    answer: "Yes. It's licensed under the MIT license."
+  }
+]
+
 
 export default function Page() {
+
   return (
     <div>
-      <h1>Hello Page</h1>
+      <Accordion type="single" className="w-full">
+        {
+          items.map(item => (
+            <AccordionItem value={item.id} key={item.id}>
+              <AccordionTrigger>{item.question}</AccordionTrigger>
+              <AccordionContent>
+                {item.answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))
+        }
+      </Accordion>
     </div>
   );
 }
