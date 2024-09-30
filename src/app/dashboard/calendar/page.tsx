de007ff38d246e6f4cee3c144e062f952637e610
Mauricio Matango <ReedRichards0511>
2024-09-30 10:19:41 -0500

    add simple contador

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -3,10 +3,12 @@
 
 import { useState } from "react";
 import { Calendar } from "@/components/ui/calendar"
+import { Button } from "@/components/ui/button";
 
 
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [count, setCount] = useState<number>(0);
 
   const smallDate = date?.toLocaleString("es-ES", {
     weekday: "short",
@@ -23,23 +25,15 @@ export default function Page() {
         className="rounded-md border shadow"
         disabled={(date) =>date.getDay() === 0}
       />
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border shadow"
-      />
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border shadow"
-      />
-      <div>
+      <Button variant='default'>Aumentar contador</Button>
+      <Button variant='secondary'>Disminuir contador</Button>
+      <Button variant='outline'>Resetear contador</Button>
+      <h1 className="text-3xl">Valor: {count}</h1>
+      {/* <div>
         <h1 className="text-3xl">Informacion</h1>
         <div className="border-b"></div>
         <p>{smallDate}</p>
-      </div>
+      </div> */}
     </div>
   )
 }
