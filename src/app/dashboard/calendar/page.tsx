af209d1fdc887388b5439dca4766d12efd8c35be
Mauricio Matango <ReedRichards0511>
2024-10-03 12:27:47 -0500

    add multiple calendars

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -3,10 +3,13 @@
 
 import { useState } from "react";
 import { Calendar } from "@/components/ui/calendar"
+import { DateRange } from "react-day-picker";
 
 
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [multipleDate, setMultipleDate] = useState<Date[] | undefined>([]);
+  const [rangeDates, setRangeDates] = useState<DateRange | undefined>();
 
   const smallDate = date?.toLocaleString("es-ES", {
     weekday: "short",
@@ -21,12 +24,34 @@ export default function Page() {
         selected={date}
         onSelect={setDate}
         className="rounded-md border shadow"
-        disabled={(date) =>date.getDay() === 0}
+        disabled={(date) =>date.getDay() === 0 || date.getDay() === 6}
+      />
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border shadow"
+        disabled={(date) =>date > new Date()}
+      />
+      <Calendar
+        mode="multiple"
+        selected={multipleDate}
+        onSelect={setMultipleDate}
+        className="rounded-md border shadow"
+      />
+      <Calendar
+        mode="range"
+        selected={rangeDates}
+        onSelect={setRangeDates}
+        className="rounded-md border shadow"
       />
       <div>
         <h1 className="text-3xl">Informacion</h1>
         <div className="border-b"></div>
         <p>{smallDate}</p>
+        <p>{multipleDate?.map(date => date.toLocaleDateString()).join(", ")}</p>
+        <p>Desde: {rangeDates?.from?.toLocaleDateString()}</p>
+        <p>Hasta: {rangeDates?.to?.toLocaleDateString()}</p>
       </div>
     </div>
   )
