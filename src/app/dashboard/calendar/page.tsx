de10c14e158971dcaaaefbe129b950341c03de52
Mauricio Matango <ReedRichards0511>
2024-10-03 11:42:59 -0500

    remove button counter

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -3,12 +3,10 @@
 
 import { useState } from "react";
 import { Calendar } from "@/components/ui/calendar"
-import { Button } from "@/components/ui/button";
 
 
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date());
-  const [count, setCount] = useState<number>(0);
 
   const smallDate = date?.toLocaleString("es-ES", {
     weekday: "short",
@@ -25,15 +23,11 @@ export default function Page() {
         className="rounded-md border shadow"
         disabled={(date) =>date.getDay() === 0}
       />
-      <Button variant='default'>Aumentar contador</Button>
-      <Button variant='secondary'>Disminuir contador</Button>
-      <Button variant='outline'>Resetear contador</Button>
-      <h1 className="text-3xl">Valor: {count}</h1>
-      {/* <div>
+      <div>
         <h1 className="text-3xl">Informacion</h1>
         <div className="border-b"></div>
         <p>{smallDate}</p>
-      </div> */}
+      </div>
     </div>
   )
 }
