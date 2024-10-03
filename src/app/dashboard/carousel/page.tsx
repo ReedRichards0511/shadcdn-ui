acc3334d193dd9bf11a230c755329f07976bfbaa
Mauricio Matango <ReedRichards0511>
2024-10-03 15:36:42 -0500

    add autoplay

---
+++ src/app/dashboard/carousel/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import * as React from "react"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+
+
+export default function Page() {
+  return (
+    <div className="w-full flex justify-center ">
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+          dragFree: true
+        }}
+        className="w-full max-w-sm"
+        plugins={[Autoplay({delay:1000})]}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-3xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className=" hidden sm:flex justify-center items-center" />
+        <CarouselNext className=" hidden sm:block"/>
+      </Carousel>
+    </div>
+  );
+}
